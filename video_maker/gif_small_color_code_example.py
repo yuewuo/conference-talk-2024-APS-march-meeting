2c077dd5c346e9a122b7f3ffc2764b65dd094ab0
Yue Wu <yue.wu@yale.edu>
2024-03-04 16:50:57 -0600

    add gif

---
+++ video_maker/gif_small_color_code_example.py
@@ -0,0 +1,15 @@
+import imageio
+
+NAME = "small_color_code_example"
+FRAME_RATE = 60
+FRAME_PER_SEC = 6
+DURATION = 30
+SLOWER = 3.0
+
+FOLDER = f"screenshots_{NAME}"
+INTERVAL = FRAME_RATE // FRAME_PER_SEC
+
+images = []
+for index in range(DURATION * FRAME_PER_SEC):
+    images.append(imageio.v2.imread(f"{FOLDER}/{index * INTERVAL}.png"))
+imageio.mimsave(f'{NAME}.gif', images, duration=DURATION * SLOWER)

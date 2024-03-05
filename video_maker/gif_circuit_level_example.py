e98daebff388d42277393b7143cb8037f290efc6
Yue Wu <yue.wu@yale.edu>
2024-03-05 09:33:57 -0600

    update gif for infinite loop

--- video_maker/gif_circuit_level_example.py
+++ video_maker/gif_circuit_level_example.py
@@ -2,8 +2,8 @@ import imageio
 
 NAME = "circuit_level_example"
 FRAME_RATE = 60
-FRAME_PER_SEC = 1 / 0.15
-DURATION = 2
+FRAME_PER_SEC = 1 / 0.15 / 2
+DURATION = 1.2
 SLOWER = 100.0
 
 FOLDER = f"screenshots_{NAME}"
@@ -12,4 +12,4 @@ INTERVAL = int(FRAME_RATE / FRAME_PER_SEC)
 images = []
 for index in range(int(DURATION * FRAME_PER_SEC)):
     images.append(imageio.v2.imread(f"{FOLDER}/{index * INTERVAL}.png"))
-imageio.mimsave(f'{NAME}.gif', images, duration=DURATION * SLOWER)
+imageio.mimsave(f'{NAME}.gif', images, duration=DURATION * SLOWER, loop=65535)
